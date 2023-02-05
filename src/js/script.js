be904771804012f10ae8b3c4953ce3c8267d7416
gomes3654gui@gmail.com <gomes3654gui@gmail.com>
2023-02-05 16:56:54 -0300

    Jogo da Velha

---
+++ src/js/script.js
@@ -0,0 +1,93 @@
+const xClass = 'X'
+const oClass = 'O'
+let xTurn
+
+const cells = document.querySelectorAll('.cell')
+const board = document.querySelector('#board')
+
+const gameEndMessage = document.querySelector('[data-game-end-message]')
+const gameEndElement = document.querySelector('#gameEndElement')
+
+const restartButton = document.querySelector('#restartButton')
+
+const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
+
+startGame();
+
+function startGame() {
+    xTurn = true
+    board.classList.add('X')
+    
+    cells.forEach(cell => {
+        cell.classList.remove(xClass)
+        cell.classList.remove(oClass)
+        board.classList.replace('O', 'X')
+
+        cell.addEventListener('click', handleClick, { once: true })
+    })
+
+    restartButton.addEventListener('click', startGame)
+
+    gameEndElement.classList.remove('show')
+    document.querySelector('main').classList.remove('end')
+}
+
+function handleClick(e) {
+    let cell = e.target
+    let turnClass = xTurn ? xClass : oClass
+
+    placeMark(cell, turnClass);
+
+    if(checkWin (turnClass)) {
+        endGame(false);
+    } else if (checkDraw()) {
+        endGame(true);
+    }
+    swapTurn();
+    setBoardHover();
+}
+
+function placeMark(cell, turnClass) {
+    cell.classList.add(turnClass)
+}
+
+function swapTurn() {
+    xTurn = !xTurn
+}
+
+function setBoardHover() {
+    xTurn ? board.classList.replace('O', 'X') : board.classList.replace('X', 'O')
+}
+
+function checkWin (turnClass) {
+    return lines.some(line => {
+        return line.every(index => {
+            return cells[index].classList.contains(turnClass)
+        })
+    })
+}
+
+function checkDraw() {
+    return [...cells].every(cell => {
+        return cell.classList.contains(xClass) || cell.classList.contains(oClass);
+    })
+}
+
+function endGame(draw) {
+    if(draw) {
+        gameEndMessage.innerText = 'Empate !';
+    } else {
+        gameEndMessage.innerText = `${xTurn ? "X's" : "O's"} Venceu !`;
+    }
+    gameEndElement.classList.add('show')
+    document.querySelector('main').classList.add('end')
+}
